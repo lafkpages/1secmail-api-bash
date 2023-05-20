d2377c61d68081cd605a8e63e765ac087728ee09
LuisAFK <soy.lafk+gh@gmail.com>
2023-05-20 16:09:10 +0000

    Initial commit

---
+++ 1secmail.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if command -v jq > /dev/null 2>&1; then :; else
+  echo "jq not installed" 1>&2
+  exit 3
+fi
+
+usage() {
+  echo "Usage: $0 [-R count?]" 1>&2
+  exit 2
+}
+
+if [ -z "$*" ]; then
+  usage
+fi
+
+API="https://www.1secmail.com/api/v1/?action"
+
+while getopts "R:" opt; do
+  case "$opt" in
+    R)
+      count="$OPTARG"
+      if [ -z "$count" ]; then
+        count="1"
+      fi
+
+      curl -s "$API=genRandomMailbox&count=$count" | jq -r '.[]'
+      ;;
+
+    *)
+      usage
+      ;;
+  esac
+done
